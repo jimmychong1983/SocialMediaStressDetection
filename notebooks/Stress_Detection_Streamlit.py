dec5c1a952c0d6e3cf21efe6eac604b8cd251aae
jimmychong1983 <jimmyjc2001@gmail.com>
2024-09-29 09:19:02 +0800

    include requirement.txt

--- notebooks/Stress_Detection_Streamlit.py
+++ notebooks/Stress_Detection_Streamlit.py
@@ -22,15 +22,3 @@ if st.button('Predict'):
         st.write("The text does not indicate high stress levels.")
     else:
         st.write("The text indicates high stress levels.")
-
-# Define the main function
-def main():
-    # Call the prediction function
-    prediction = predict([sentence])
-
-    # Display the prediction result
-    st.write("Prediction:", prediction)
-
-# Run the main function
-if __name__ == '__main__':
-    main()
